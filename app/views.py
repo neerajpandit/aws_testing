b333cc3f69cf0759f3b33b2b5f48f9062d9129b0
neerajpandit <pandeyneeraj1507@gmail.com>
2023-11-09 23:14:22 +0530

    1st

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views import generic
+from .models import Post
+
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'app/index.html'
